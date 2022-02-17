01e701756800594841dc5fc6e02439ac3112fa4c
Cassandra-White <kriss.alex78@gmail.com>
2022-02-17 15:14:41 +0100

    Gestion de Component Global et Local / Slot / Dynamic Component / Teleport

---
+++ 2- sandBox/project-5/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <button @click="isActive('active-menu')">Active Menu 1</button>
+    <button @click="isActive('second-active-menu')">Active Menu 2</button>
+
+    <keep-alive>
+       <component :is="menuSelected"></component>
+    </keep-alive>
+
+    <!-- <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info> -->
+  </div>
+</template>
+
+<script>
+// import TheHeader from './components/TheHeader.vue';
+// import BadgeList from './components/BadgeList.vue';
+// import UserInfo from './components/UserInfo.vue';
+import ActiveMenu from './components/ActiveMenu.vue';
+import SecondActiveMenu from './components/SecondActiveMenu.vue';
+
+
+export default {
+  components:{
+    // TheHeader : TheHeader,
+    // BadgeList: BadgeList,
+    // UserInfo: UserInfo,
+    ActiveMenu,
+    SecondActiveMenu,
+
+  },
+  data() {
+    return {
+
+      menuSelected: 'active-menu',
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+  methods:{
+    isActive(item){
+      this.menuSelected = item;
+
+    },
+  }
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ 2- sandBox/project-5/src/components/SecondActiveMenu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Active Menu 2 :</h1>
+    <input type="text" ref="goal" />
+    <button @click="send">send</button>
+    <teleport to="#app">
+        <error-alerte v-if="inputIsInvalid">
+        <h2>Input is Invalid</h2>
+        <p>Input can't be empty</p>
+        <button @click="confirmError">Okey !!</button>
+        </error-alerte>
+    </teleport>
+  </div>
+</template>
+
+<script>
+import ErrorAlerte from "./ErrorAlerte.vue";
+
+export default {
+  components: {
+    ErrorAlerte,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+
+  methods: {
+    send() {
+      const msgSend = this.$refs.goal.value;
+
+      if (msgSend === "") this.inputIsInvalid = true;
+      else this.inputIsInvalid = false;
+    },
+    confirmError(){
+        this.inputIsInvalid = false
+    },
+  },
+};
+</script>
